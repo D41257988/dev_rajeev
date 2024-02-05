627a33df39982953614ef0be82df75c2a7f290c6
Rajeev <147071216+D41255028@users.noreply.github.com>
2024-02-05 15:42:28 -0500

    updated the case filter

--- wldn/sp/sp_wldn_case_service_unclosed_sla.sql
+++ wldn/sp/sp_wldn_case_service_unclosed_sla.sql
@@ -70,7 +70,7 @@ with unique_case as (
     cast(`utility.udf_convert_UTC_to_EST`(CH.created_date) as date) >= v_startdate and cast(`utility.udf_convert_UTC_to_EST`(CH.created_date) as date) < v_enddate
     and C.record_type_id in ('012o00000012bKoAAI', '012o00000012ZrjAAE', '0121N0000019B0JQAU', '0121N000000uGPJQA2', '0121N000001AQG8QAO', '0121N000000qslUQAQ', '0121N000000qslTQAQ')
     and C.status != 'Closed'
-    and C.institution_brand_c = 'a0ko0000002BSH4AAO'
+    and (C.institution_brand_c in ('a0kDP000008l7bvYAA') OR C.institution_code_c = 3.0)
     and C.is_deleted=false
 
 ),
